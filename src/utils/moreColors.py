0890cd15a8e087c3f97de893f74235de95816544
amadea-system <53259552+amadea-system@users.noreply.github.com>
2020-05-01 17:53:52 -0400

    Add gg_purple function for purple color

--- src/utils/moreColors.py
+++ src/utils/moreColors.py
@@ -2,6 +2,8 @@
 Provides convenience functions for the various custom colors used by GG.
 Colors include:
     GG Dark Green
+    GG Light Green
+    GG Purple
 
 Part of the Gabby Gums Discord Logger.
 """
@@ -19,6 +21,11 @@ def gabby_gums_light_green() -> Colour:
     """A convenience function that returns a :class:`Colour` with a value of 0xA5B8A5 (R165, G184, B165)."""
     # discord.Color.from_rgb(80, 135, 135))
     return Colour(0xA5B8A5)
+
+
+def gabby_gums_purple() -> Colour:
+    """A convenience function that returns a :class:`Colour` with a value of 0x9932CC."""
+    return Colour(0x9932CC)
 
 
 
