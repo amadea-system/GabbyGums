657e3a791b5f884a0695ea61de60d8600eb2f3c8
amadea-system <53259552+amadea-system@users.noreply.github.com>
2020-05-07 13:02:30 -0400

    Update /msg/ PK API usage to the V1 API Endpoint. Remove lower some logging to debug level. Update comments/cleaned up code a bit.

--- src/utils/pluralKit.py
+++ src/utils/pluralKit.py
@@ -1,6 +1,8 @@
 """
 Functions for interfacing with the Plural Kit API.
-API Functions include:
+API Endpoint functions include:
+    get_pk_system_from_userid -> /a/
+    get_pk_message -> /msg/
 
 Part of the Gabby Gums Discord Logger.
 """
@@ -26,24 +28,21 @@ class UnknownPKError(Exception):
 
 
 async def get_pk_system_from_userid(user_id: int) -> Optional[Dict]:
-
+    """Gets a PK system from the PluralKit API using a Discord UserID"""
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(f'https://api.pluralkit.me/v1/a/{user_id}') as r:
                 if r.status == 200:  # We received a valid response from the PK API.
-                    logging.info(f"User has an associated PK Account linked to thier Discord Account.")
+                    logging.debug(f"User has an associated PK Account linked to their Discord Account.")
 
                     # Convert the JSON response to a dict
                     pk_response = await r.json()
+                    logging.debug(f"Got system: {pk_response}")
 
-                    # Unpack and return.
-                    logging.info(f"Got system: {pk_response}")
-                    # system_id = pk_response['id']
-                    # return system_id
                     return pk_response
                 elif r.status == 404:
                     # No PK Account found.
-                    log.info("No PK Account found.")
+                    log.debug("No PK Account found.")
                     return None
                 else:
                     raise UnknownPKError(f"Received Status Code: {r.status} ({r.reason}) for /a/{user_id}")
@@ -56,9 +55,8 @@ async def get_pk_message(message_id: int) -> Optional[Dict]:
     """Attempts to retrieve details on a proxied/pre-proxied message"""
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://api.pluralkit.me/msg/{}'.format(message_id)) as r:
+            async with session.get('https://api.pluralkit.me/v1/msg/{}'.format(message_id)) as r:
                 if r.status == 200:  # We received a valid response from the PK API. The message is probably a pre-proxied message.
-                    # TODO: Remove logging once bugs are worked out.
                     logging.debug(f"Message {message_id} is still on the PK api.")
                     # Convert the JSON response to a dict, Cache the details of the proxied message, and then bail.
                     pk_response = await r.json()
